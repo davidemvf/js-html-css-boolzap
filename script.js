bacd4a716eb2cc369ee7998075d01e4afd8d95aa
davidemvf <davidemv.fiore@gmail.com>
2019-07-22 22:02:46 +0200

    Messaggi dell'utente visualizzabili nel box

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("Ciao");
+$(document).ready(function() {
+
+  $(".fas").click(function() {
+    var inputext = $("#text-message").val();
+
+    $(".messages-centering").append(
+      '<div class="user-container">' + '<div class="user">' + inputext + '</div>' + '</div>'
+    );
+    //
+    // $("#text-message").val("");
+    // var message = $(".user-container").clone();
+    // message.text(inputext);
+    // $(".messages-centering").append(message);
+
+     $("#text-message").val("");
+
+  })
+});
