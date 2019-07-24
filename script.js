bae11c31e6a2fe0b56053126b5a3c3a0bd61a1c3
davidemvf <davidemv.fiore@gmail.com>
2019-07-24 13:41:26 +0200

    Implementato il filtro per la ricerca dei contatti. Da risolvere la comparsa del font aeroplanino quando si digita il messaggio

--- script.js
+++ script.js
@@ -16,10 +16,8 @@ $(document).ready(function() {
       '<div class="user-container">' + '<div class="user">' + inputext + '</div>' + '</div>'
     );
     //
-    // $("#text-message").val("");
-    // var message = $(".user-container").clone();
-    // message.text(inputext);
-    // $(".messages-centering").append(message);
+    $("#text-message").val("");
+    
 
      setTimeout(answer, 1000);
 
@@ -29,5 +27,28 @@ $(document).ready(function() {
        $(".messages-centering").append(reply);
      }
 
+  })
+
+  //funzione per la ricerca dei contatti
+
+  // la mia funzione partirà nel momento in cui comincio a digitare all'interno dell'inputext
+  $("#search-bar").keyup(function() {
+    // salvo il contenuto digitato con una variabile
+    var letter = $(this).val().toLowerCase();
+    //faccio in modo che il contenuto dell'input sia tutto in minuscolo
+    // letter.toLowerCase();
+    console.log(letter);
+
+    // a questo punto parte una funzione che controlla i contatti
+    $(".past-chat").each(function() {
+      $(this).show();
+
+      var contatto = $(this).find(".name").text().toLowerCase();
+      console.log(contatto);
+
+      if (!contatto.includes(letter)) {
+        $(this).hide();
+      }
+    })
   })
 });
